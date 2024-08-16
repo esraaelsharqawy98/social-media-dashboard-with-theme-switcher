fbacb8a14e89ecd5f26dc3078572bcd73fa42a52
esraaelsharqawy98 <esraaelsharqawy998@gmail.com>
2024-08-16 23:35:23 +0300

    edit project

--- src/Components/EngagementsWrapper/EngagementsWrapper.jsx
+++ src/Components/EngagementsWrapper/EngagementsWrapper.jsx
@@ -2,16 +2,82 @@ import React, { useState, useEffect } from "react";
 import EngagementItem from "../EngagementItem/EngagementItem";
 import "./EngagementsWrapper.css";
 function EngagementsWrapper() {
-  const [engagements, setEngagement] = useState([]);
+  const [engagements, setEngagement] = useState(
+    [
+      {
+          "id":1,
+          "platform": "facebook",
+          "metric": "Page Views",
+          "value": 87,
+          "change": "3%"
+      },
+      {
+          "id":2,
+          "platform": "facebook",
+          "metric": "Likes",
+          "value": 52,
+          "change": "2%",
+          "isup":true
+      },
+      {   
+          "id":3,
+          "platform": "instagram",
+          "metric": "Likes",
+          "value": 5462,
+          "change": "2257%",
+          "isup":false
+      },
+      {
+          "id":4,
+          "platform": "instagram",
+          "metric": "Profile Views",
+          "value": 52000,
+          "change": "1375%",
+          "isup":true
+      },
+      {
+          "id":5,
+          "platform": "twitter",
+          "metric": "Retweets",
+          "value": 117,
+          "change": "303%",
+          "isup":true
+      },
+      { 
+          "id":6,
+          "platform": "twitter",
+          "metric": "Likes",
+          "value": 507,
+          "change": "553%",
+          "isup":true
+      },
+      {
+          "id":7,
+          "platform": "youtube",
+          "metric": "Likes",
+          "value": 107,
+          "change": "19%",
+          "isup":false
+      },
+      {
+          "id":8,
+          "platform": "youtube",
+          "metric": "Total Views",
+          "value": 1407,
+          "change": "12%",
+          "isup":false
+      }
+  ]
+  );
 
-  useEffect(() => {
-    function callApi() {
-      fetch("http://localhost:3000/engagement")
-        .then((response) => response.json())
-        .then((finalResult) => setEngagement(finalResult));
-    }
-    callApi();
-  }, []);
+  // useEffect(() => {
+  //   function callApi() {
+  //     fetch("http://localhost:3000/engagement")
+  //       .then((response) => response.json())
+  //       .then((finalResult) => setEngagement(finalResult));
+  //   }
+  //   callApi();
+  // }, []);
 
   return (
     <div className="engagementsWrapper">
--- src/Components/FollowersWrapper/FollowersWrapper.jsx
+++ src/Components/FollowersWrapper/FollowersWrapper.jsx
@@ -3,16 +3,55 @@ import './FollowersWrapper.css'
 import FollowerItem from "../FollowerItem/FollowerItem";
 
 function FollowersWrapper() {
-  const [followers, setFollowers] = useState([]);
+  const [followers, setFollowers] = useState(
+    [
+      {
+          "id":1,
+          "platform": "facebook",
+          "username": "@nathanf",
+          "metric": "Followers",
+          "value": 1987,
+          "change": "12 Today",
+          "isup":true
+      },
+      {
+          "id":2,
+          "platform": "twitter",
+          "username": "@nathanf",
+          "metric": "Followers",
+          "value": 1044,
+          "change": "99 Today",
+          "isup":true
+      },
+      {
+          "id":3,
+          "platform": "instagram",
+          "username": "@realnathanf",
+          "metric": "Followers",
+          "value": 11000,
+          "change": "1099 Today",
+          "isup":true
+      },
+      {
+          "id":4,
+          "platform": "youtube",
+          "username": "Nathan F.",
+          "metric": "Subscribers",
+          "value": 8239,
+          "change": "144 Today",
+          "isup":false
+      }
+  ]
+  );
 
-  useEffect(() => {
-    function callApi() {
-      fetch("http://localhost:3000/followers")
-        .then((response) => response.json())
-        .then((finalResult) => setFollowers(finalResult));
-    }
-    callApi();
-  }, []);
+  // useEffect(() => {
+  //   function callApi() {
+  //     fetch("http://localhost:3000/followers")
+  //       .then((response) => response.json())
+  //       .then((finalResult) => setFollowers(finalResult));
+  //   }
+  //   callApi();
+  // }, []);
 
   return (
     <div className="followers-list">
